27e510ca3154df45d8fc9b3c92297bd07ee7ab44
Liyb2002 <yl4092@columbia.edu>
2022-11-04 14:34:06 -0400

    clean2

--- pyrender/example.py
+++ pyrender/example.py
@@ -14,14 +14,6 @@ from pyrender import PerspectiveCamera,\
 
 
 #------------------------------------------------------------------------------
-# Creating meshes with per-vertex colors
-#------------------------------------------------------------------------------
-boxv_trimesh = trimesh.creation.box(extents=0.1*np.ones(3))
-boxv_vertex_colors = np.random.uniform(size=(boxv_trimesh.vertices.shape))
-boxv_trimesh.visual.vertex_colors = boxv_vertex_colors
-boxv_mesh = Mesh.from_trimesh(boxv_trimesh, smooth=False)
-
-#------------------------------------------------------------------------------
 # Creating meshes with per-face colors
 #------------------------------------------------------------------------------
 boxf_trimesh = trimesh.creation.box(extents=0.1*np.ones(3))
@@ -29,21 +21,11 @@ boxf_face_colors = np.random.uniform(size=boxf_trimesh.faces.shape)
 boxf_trimesh.visual.face_colors = boxf_face_colors
 boxf_mesh = Mesh.from_trimesh(boxf_trimesh, smooth=False)
 
-#------------------------------------------------------------------------------
-# Creating meshes from point clouds
-#------------------------------------------------------------------------------
-points = trimesh.creation.icosphere(radius=0.05).vertices
-point_colors = np.random.uniform(size=points.shape)
-points_mesh = Mesh.from_points(points, colors=point_colors)
-
 #==============================================================================
 # Light creation
 #==============================================================================
 
 direc_l = DirectionalLight(color=np.ones(3), intensity=1.0)
-spot_l = SpotLight(color=np.ones(3), intensity=10.0,
-                   innerConeAngle=np.pi/16, outerConeAngle=np.pi/6)
-point_l = PointLight(color=np.ones(3), intensity=10.0)
 
 #==============================================================================
 # Camera creation
@@ -70,16 +52,16 @@ scene = Scene(ambient_light=np.array([0.02, 0.02, 0.02, 1.0]))
 #------------------------------------------------------------------------------
 # By manually creating nodes
 #------------------------------------------------------------------------------
-boxv_node = Node(mesh=boxv_mesh, translation=np.array([-0.1, 0.10, 0.05]))
-scene.add_node(boxv_node)
 boxf_node = Node(mesh=boxf_mesh, translation=np.array([-0.1, -0.10, 0.05]))
+boxf_node2 = Node(mesh=boxf_mesh, translation=np.array([-0.2, -0.20, 0.5]))
+
 scene.add_node(boxf_node)
+scene.add_node(boxf_node2)
 
 #------------------------------------------------------------------------------
 # By using the add() utility function
 #------------------------------------------------------------------------------
 direc_l_node = scene.add(direc_l, pose=cam_pose)
-spot_l_node = scene.add(spot_l, pose=cam_pose)
 
 #==============================================================================
 # Using the viewer with a default camera
