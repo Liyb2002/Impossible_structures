b11258028c153e5669f5a3648517d6c3ce5756e5
Liyb2002 <yl4092@columbia.edu>
2022-11-07 23:27:28 -0500

    smaller box

--- pyrender/example.py
+++ pyrender/example.py
@@ -5,6 +5,7 @@ pyglet.options['shadow_window'] = False
 import os
 import numpy as np
 import trimesh
+import math
 
 from pyrender import PerspectiveCamera,\
                      DirectionalLight, SpotLight, PointLight,\
@@ -16,8 +17,8 @@ from pyrender import PerspectiveCamera,\
 #------------------------------------------------------------------------------
 # Creating meshes with per-face colors
 #------------------------------------------------------------------------------
-boxf_trimesh = trimesh.creation.box(extents=0.1*np.ones(3))
-boxf_face_colors = np.random.uniform(size=boxf_trimesh.faces.shape)
+boxf_trimesh = trimesh.creation.box(extents=0.01*np.ones(3))
+boxf_face_colors = (0.3,0.5,0.2,1)
 boxf_trimesh.visual.face_colors = boxf_face_colors
 boxf_mesh = Mesh.from_trimesh(boxf_trimesh, smooth=False)
 
@@ -32,8 +33,40 @@ direc_l = DirectionalLight(color=np.ones(3), intensity=1.0)
 #==============================================================================
 
 cam = PerspectiveCamera(yfov=(np.pi / 3.0))
+width = 500
+height = 500
+far = 1000.0
+near = 0.1
+c = -near/far
+
+theta_w = math.radians(45)
+theta_h = math.radians(45)
+
+S_xyz = np.array([
+    [1/far * np.tan(theta_w/2), 0, 0, 0],
+    [0, 1/far * np.tan(theta_h/2), 0, 0],
+    [0, 0, 1/far, 0],
+    [0, 0, 0, 1]
+])
+
+M_pp = np.array([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0,0,1/(1+c), -c/(1+c)],
+    [0,0,-1,0]
+])
+
+M_reshape = np.array([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0,0,-2,-1],
+    [0,0,0,1]
+])
+
+perspective_matrix = M_reshape @ M_pp @ S_xyz
+
 cam_pose = np.array([
-    [0.0,  -np.sqrt(2)/2, np.sqrt(2)/2, 0.5],
+    [0.0,  -np.sqrt(4)/2, np.sqrt(2)/2, 0.5],
     [1.0, 0.0,           0.0,           0.0],
     [0.0,  np.sqrt(2)/2,  np.sqrt(2)/2, 0.4],
     [0.0,  0.0,           0.0,          1.0]
@@ -52,11 +85,14 @@ scene = Scene(ambient_light=np.array([0.02, 0.02, 0.02, 1.0]))
 #------------------------------------------------------------------------------
 # By manually creating nodes
 #------------------------------------------------------------------------------
-boxf_node = Node(mesh=boxf_mesh, translation=np.array([-0.1, -0.10, 0.05]))
-boxf_node2 = Node(mesh=boxf_mesh, translation=np.array([-0.2, -0.20, 0.5]))
+boxf_node = Node(mesh=boxf_mesh, translation=np.array([0, 0, 0]))
+boxf_node2 = Node(mesh=boxf_mesh, translation=np.array([0.1, 0, 0]))
+boxf_node3 = Node(mesh=boxf_mesh, translation=np.array([0, 0.1, 0]))
 
 scene.add_node(boxf_node)
 scene.add_node(boxf_node2)
+scene.add_node(boxf_node3)
+
 
 #------------------------------------------------------------------------------
 # By using the add() utility function
