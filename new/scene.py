5a73b93ca59ac76b19db1d49c3a23f36bf14eb83
Liyb2002 <yl4092@columbia.edu>
2022-11-12 16:52:38 -0500

    work

--- new/scene.py
+++ new/scene.py
@@ -111,7 +111,7 @@ class Camera:
 
 
 class Scene:
-    def __init__(self, voxel_edges=0.06, exposure=3):
+    def __init__(self, voxel_edges=0, exposure=3):
         ti.init(arch=ti.vulkan)
         print(HELP_MSG)
         self.window = ti.ui.Window("Taichi Voxel Renderer",
