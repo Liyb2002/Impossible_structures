ca1ebaee43317d6d2a70e1b9e98a8d637716384a
Liyb2002 <yl4092@columbia.edu>
2022-11-27 15:54:07 -0500

    three

--- raytracer/generate.py
+++ raytracer/generate.py
@@ -10,7 +10,7 @@ import metrics
 import intersection
 import particle
 
-from ray_tracing_models import Camera, Hittable_list, PI, xy_rect, xz_rect, yz_rect, diffuse, rectangle
+from ray_tracing_models import Camera, Hittable_list, PI, xy_rect, xz_rect, yz_rect, diffuse
 ti.init(arch=ti.gpu)
 
 PI = 3.14159265
