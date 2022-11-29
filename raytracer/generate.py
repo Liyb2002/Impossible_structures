c2fc66e3cdae292e7f00b05db07e4b4e0c4a147a
Liyb2002 <yl4092@columbia.edu>
2022-11-29 01:52:35 -0500

    clean up

--- raytracer/generate.py
+++ raytracer/generate.py
@@ -11,7 +11,6 @@ import metrics
 import intersection
 import particle
 
-from ray_tracing_models import Camera, Hittable_list, PI, xy_rect, xz_rect, yz_rect, diffuse
 ti.init(arch=ti.gpu)
 
 PI = 3.14159265
@@ -22,46 +21,9 @@ image_width = 800
 image_height = int(image_width / aspect_ratio)
 canvas = ti.Vector.field(3, dtype=ti.f32, shape=(image_width, image_height))
 
-# Rendering parameters
-max_depth = 10
-
-@ti.kernel
-def render():
-    for i, j in canvas:
-        u = (i) / image_width
-        v = (j) / image_height
-        color = ti.Vector([0.0, 0.0, 0.0])
-        ray = camera.get_ray(u, v)
-        camera_pos = camera.get_camera_origin()
-        color += ray_color(ray, camera_pos)
-        canvas[i, j] += color
-
-
-def create_rect(start_x, start_y, start_z, x_len, y_len, z_len,r,g,b):
-    # scene.add(xy_rect(_x0=start_x, _x1=start_x+x_len, _y0=start_y, _y1=start_y+y_len, _k=start_z, material=1, color=ti.Vector([r, g, b])))
-    scene.add(xy_rect(_x0=start_x, _x1=start_x+x_len, _y0=start_y, _y1=start_y+y_len, _k=start_z+z_len, color=ti.Vector([r, g, b])))
-    
-    # # scene.add(xz_rect(_x0=start_x, _x1=start_x+x_len, _z0=start_z, _z1=start_z+z_len, _k=start_y, material=1, color=ti.Vector([0.2, 0.4, 0.5])))
-    scene.add(xz_rect(_x0=start_x, _x1=start_x+x_len, _z0=start_z, _z1=start_z+z_len, _k=start_y+y_len, color=ti.Vector([0.8, 0.8, 0.5])))
-
-    # # scene.add(yz_rect(_y0=start_y, _y1=start_y+y_len, _z0=start_z, _z1=start_z+z_len, _k=start_x, material=1, color=ti.Vector([0.3, 0.3, 0.2])))
-    scene.add(yz_rect(_y0=start_y, _y1=start_y+y_len, _z0=start_z, _z1=start_z+z_len, _k=start_x+x_len, color=ti.Vector([0.3, 0.3, 0.9])))
-
-@ti.func
-def ray_color(ray, camera_pos):
-    color_buffer = ti.Vector([1.0, 1.0, 1.0])
-    curr_origin = camera_pos
-    curr_direction = ray
-    is_hit, hit_point, hit_point_normal, front_face, color = scene.hit(curr_origin, curr_direction)
-    if is_hit:
-        color_buffer = diffuse(curr_direction, hit_point, hit_point_normal, color)
-    return color_buffer
 
 if __name__ == "__main__":
 
-    scene = Hittable_list()
-
-    camera = Camera()
     gui = ti.GUI("Ray Tracing", res=(image_width, image_height))
     canvas.fill(0)
     cnt = 0
@@ -190,24 +152,3 @@ if __name__ == "__main__":
         json.dump(result, f, indent=2)
 
 
-    # for cc in result_particle.connecting_comp:
-    #     i = cc.get_object()
-    #     create_rect(i.start_x, i.start_y, i.start_z, i.scale_x, i.scale_y, i.scale_z, 0.2, 0.4, 0.8)
-
-    # f_struct = result_particle.foreground_structure
-    # for i in f_struct.rect:
-    #     create_rect(i.start_x, i.start_y, i.start_z, i.scale_x, i.scale_y, i.scale_z, 0.2, 0.4, 0.8)
-    
-    # b_struct = result_particle.background_structure
-    # for i in b_struct.rect:
-    #     create_rect(i.start_x, i.start_y, i.start_z, i.scale_x, i.scale_y, i.scale_z, 0.2, 0.9, 0.3)
-
-    # d_struct = result_particle.dummy_structure
-    # for i in d_struct.rect:
-    #     create_rect(i.start_x, i.start_y, i.start_z, i.scale_x, i.scale_y, i.scale_z, 0.9, 0.1, 0.1)
-
-    # while gui.running:
-    #     render()
-    #     cnt += 1
-    #     gui.set_image(np.sqrt(canvas.to_numpy() / cnt))
-    #     gui.show()
