f30f12662328f3a9bac88fadb82e67efa6d04a8e
Liyb2002 <yl4092@columbia.edu>
2022-12-12 01:09:34 -0500

    seeds

--- raytracer/gen_seed.py
+++ raytracer/gen_seed.py
@@ -1,16 +1,46 @@
 import numpy as np
+import random
 
 def get_seed(pos, block_size, portion,isFront):
     seed = pos
+    intersect_type = random.randint(1,8)
 
     if isFront:
-        for i in range(1,5):
-            tempt = np.array([pos[0],pos[1]+block_size*i,pos[2]])
-            seed = np.vstack([seed, tempt])
+        if intersect_type == 1:
+            for i in range(1,5):
+                tempt = np.array([pos[0],pos[1]+block_size*i,pos[2]])
+                seed = np.vstack([seed, tempt])
+        
+        elif intersect_type == 2:
+            for i in range(1,5):
+                tempt = np.array([pos[0],pos[1]-block_size*i,pos[2]])
+                seed = np.vstack([seed, tempt])
+        
+        elif intersect_type == 3 or intersect_type == 4:
+            for i in range(1,5):
+                tempt = np.array([pos[0]+block_size*i,pos[1],pos[2]])
+                seed = np.vstack([seed, tempt])
+        
+
+
     else:
-        for i in range(1,5):
-            tempt = np.array([pos[0]-block_size*i * portion,pos[1],pos[2]])
-            seed = np.vstack([seed, tempt])
+        if intersect_type == 1 or intersect_type == 2:
+            for i in range(1,5):
+                tempt = np.array([pos[0]-block_size*i * portion,pos[1],pos[2]])
+                seed = np.vstack([seed, tempt])
+        
+        elif intersect_type == 3:
+            for i in range(1,5):
+                tempt = np.array([pos[0],pos[1]-block_size*i * portion,pos[2]])
+                seed = np.vstack([seed, tempt])
+        
+        elif intersect_type == 4:
+            for i in range(1,5):
+                tempt = np.array([pos[0]-block_size * portion,pos[1]+block_size*i * portion,pos[2]])
+                seed = np.vstack([seed, tempt])
+
+        
+
     return seed
 
 
